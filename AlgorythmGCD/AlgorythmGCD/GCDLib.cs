97d71b0052e92367eb2449a21cb1dede18155d16
Elijah <gemar19@mail.ru>
2018-04-08 18:21:53 +0300

    with tests

---
+++ AlgorythmGCD/AlgorythmGCD/GCDLib.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorythmGCD
+{
+    /// <summary>
+    /// Class calculate GCD of any set of numbers using two different algorythms
+    /// </summary>
+    public static class GCDLib
+    {
+        #region Public methods
+        /// <summary>
+        /// Calculates GCD of any set of integers using Euclidean algorithm
+        /// </summary>
+        /// <param name="numbers">input integers</param>
+        /// <returns></returns>
+        public static uint CalcEuclideanGCD(params int[] numbers)
+        {
+            return CalcGCD(out _, EuclideanAlgorythm, numbers);
+        }
+        /// <summary>
+        /// Calculates GCD of any set of integers using Euclidean algorithm
+        /// </summary>
+        /// <param name="ticks">elapsed ticks</param>
+        /// <param name="numbers">input numbers</param>
+        /// <returns></returns>
+        public static uint CalcEuclideanGCD(out long ticks, params int[] numbers)
+        {
+            return CalcGCD(out ticks, EuclideanAlgorythm, numbers);
+        }
+        /// <summary>
+        /// Calculates GCD of any set of integers using Stein's algorithm
+        /// </summary>
+        /// <param name="numbers">input integers</param>
+        /// <returns></returns>
+        public static uint CalcSteinsGCD(params int[] numbers)
+        {
+            return CalcGCD(out _, SteinsAlgorithm, numbers);
+        }
+
+        public static uint CalcSteinsGCD(out long ticks, params int[] numbers)
+        {
+            return CalcGCD(out ticks, SteinsAlgorithm, numbers);
+        }
+        #endregion
+
+        #region Private methods
+        private static uint CalcGCD(out long ticks, Func<uint, uint, uint> algorythm, params int[] numbers)
+        {
+            if (numbers.Length < 2)
+            {
+                throw new ArgumentException("Method requires at least 2 numbers");
+            }
+
+            var sw = Stopwatch.StartNew();
+            uint gcd = (uint)Math.Abs(numbers[0]);
+            for (int i = 1; i < numbers.Length && gcd > 1; i++)
+            {
+                gcd = algorythm.Invoke(gcd, (uint)Math.Abs(numbers[i]));
+            }
+
+            sw.Stop();
+            ticks = sw.ElapsedTicks;
+
+            return gcd;
+        }
+
+        private static uint EuclideanAlgorythm(uint a, uint b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+
+            return EuclideanAlgorythm(b, a % b);
+        }
+
+        private static uint SteinsAlgorithm(uint a, uint b)
+        {
+            if (a == b)
+            {
+                return a;
+            }
+            if (a == 0)
+            {
+                return b;
+            }
+            if (b == 0)
+            {
+                return a;
+            }
+            if ((~a & 1) != 0)
+            {
+                if ((b & 1) != 0)
+                {
+                    return SteinsAlgorithm(a >> 1, b);
+                }
+                else
+                {
+                    return SteinsAlgorithm(a >> 1, b >> 1) << 1;
+                }
+            }
+
+            if ((~b & 1) != 0)
+            {
+                return SteinsAlgorithm(a, b >> 1);
+            }
+
+            if (a > b)
+            {
+                return SteinsAlgorithm((a - b) >> 1, b);
+            }
+
+            return SteinsAlgorithm((b - a) >> 1, a);
+        }
+        #endregion
+    }
+}
---
+++ AlgorythmGCD/AlgorythmGCD/GCDLibTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace AlgorythmGCD
+{
+    [TestFixture]
+    public class GCDLibTests
+    {
+        static Random rand = new Random();
+
+        [TestCase(20, 100, 0, 0, -150, -1000, 25, -5, 25, 0, 20, 105, ExpectedResult = 5)]
+        [TestCase(385, 0, -1089, -11, 121, 627, 2838, -105457, ExpectedResult = 11)]
+        [TestCase(1, 2, 3, 4, 0, 0, 1, -2, -3, -55, -111, 123, ExpectedResult = 1)]
+        [TestCase(9999, -9999, 999, 999, -999, 999999, 9, ExpectedResult = 9)]
+        [TestCase(-20, -10, -30, -10, -2300, ExpectedResult = 10)]
+        [TestCase(123, 901, 55, 2223, -124, ExpectedResult = 1)]
+        [TestCase(123, 123, 123, 123, 123, ExpectedResult = 123)]
+        [TestCase(0, -10, 0, -11, 0, 512, ExpectedResult = 0)]
+        [TestCase(0, 0, 0, 0, 0, 0, 0, ExpectedResult = 0)]
+        [TestCase(-3, -3, -3, -3, ExpectedResult = 3)]
+
+
+
+        public uint CalculateGCDTest(params int[] numbers)
+        {
+            var resultEuc = GCDLib.CalcEuclideanGCD(out long eucTicks, numbers);
+            System.Diagnostics.Debug.WriteLine($"euclidean ticks: {eucTicks}");
+
+            var resultBin = GCDLib.CalcSteinsGCD(out long binTicks, numbers);
+            System.Diagnostics.Debug.WriteLine($"binary ticks: {binTicks}");
+
+            Assert.That(resultBin == resultEuc);
+            return resultEuc;
+        }
+
+        [Test]
+        public void CalculateGCDTestAuto()
+        {
+            const uint TESTS = 50;
+
+            for (uint i = 0; i < TESTS; i++)
+            {
+                byte gcd = (byte)rand.Next(0, 256);
+                Assert.AreEqual(gcd, CalculateGCDTest(GenerateGCDNumbers(gcd)));
+            }
+
+            int[] GenerateGCDNumbers(byte gcd)
+            {
+                const int MIN = 2;
+                const int MAX = 200_000;
+
+                var numbers = new int[rand.Next(MIN, MAX)];
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    numbers[i] = rand.Next(int.MinValue / (gcd + 1), int.MaxValue / (gcd + 1)) * gcd;
+                }
+
+                return numbers;
+            }
+        }
+    }
+}
